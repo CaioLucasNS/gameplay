72f46c3b2e0ba2bb0f1407108f597bb91cf91633
Caio Lucas <caio.lucas.do.silva@accenture.com>
2022-01-23 16:25:00 -0300

    configurando fontes externas

--- App.tsx
+++ App.tsx
@@ -1,5 +1,23 @@
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  // carregamento das fontes
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded) {
+    // AppLoading segura a tela de splash
+    return <AppLoading />;
+  };
+
   return <SignIn />;
 }
