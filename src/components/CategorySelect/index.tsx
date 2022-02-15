9d7c42bc7aa35a02bdf311c3186db10e37b63153
Caio Lucas <caio.lucas.do.silva@accenture.com>
2022-02-14 23:28:37 -0300

    added Categories and selection logic

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 import { categories } from '../../utils/categories';
+import { Category } from '../Category';
 
 import { styles } from './styles';
 
-export function CategorySelect() {
+type ICategorySelect = {
+  categorySelected: string;
+  setCategory: (categoryId: string) => void;
+}
+
+export function CategorySelect({ categorySelected, setCategory }: ICategorySelect) {
   return (
     <ScrollView
       horizontal
       style={styles.container}
       showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{ paddingRight: 40 }}
+      contentContainerStyle={{ padding: 40 }}
     >
       {
-        // categories.map(category => (
-        //   <Category
-
-        //   />
-        // ))
+        categories.map(category => (
+          <Category
+            key={category.id}
+            title={category.title}
+            icon={category.icon}
+            checked={category.id === categorySelected}
+            onPress={() => setCategory(category.id)}
+          />
+        ))
       }
     </ScrollView>
-  );
+  )
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import { View } from 'react-native';
-import React from 'react';
 
 import { styles } from './styles';
 import { Profile } from '../../components/Profile';
@@ -7,6 +7,12 @@ import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 
 export const Home = () => {
+  const [category, setCategory] = useState('');
+
+  const handleCategorySelect = (categoryId: string) => {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  };
+
   return (
     <View>
       <View style={styles.header}>
@@ -15,7 +21,10 @@ export const Home = () => {
       </View>
 
       <View>
-        <CategorySelect />
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
       </View>
     </View>
   );
